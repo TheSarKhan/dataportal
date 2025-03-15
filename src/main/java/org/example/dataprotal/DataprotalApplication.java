f6263f680ba6c94be63acc9667b606759a175403
benovsebektas <126647571+benovsebektas@users.noreply.github.com>
2025-03-15 06:43:20 -0700

    Initial commit

---
+++ src/main/java/org/example/dataprotal/DataprotalApplication.java
@@ -0,0 +1,13 @@
+package org.example.dataprotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataprotalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataprotalApplication.class, args);
+    }
+
+}
